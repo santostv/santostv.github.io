777675c2db707313cb3f89796102ea0ac7356275
rgomesdn <rodrigo@iocode.com.br>
2017-02-01 12:51:04 -0200

    Correções na playlist

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,6 @@ var head = document.head;
 var script = document.createElement("script");
 script.setAttribute('src','http://publicador.everstream.com.br/ws/playlist/get_content/14/?callback=?');
 head.appendChild(script);
-
 function getData(data){
     var result = data.playlistresult
     if(data.error){
@@ -22,21 +21,35 @@ function getData(data){
         var html = '',
             playlist_title = '',
             content_title = '',
-            content_description = '';
+            content_description = '',
+            first_file = '',
+            first_cover = '';
 
         for(i in result){
             var content = result[i].content;
             for(j in content){
-                html += '<div class="content">'
-                html += '   <p class="date">18.01.2017</p>'
-                html += '   <p class="link">';
-                html += '       <a href="#pid='+content[j].content_id+'">'+content[j].content_title+'</a>';
-                html += '   </p>';
-                html += '</div>';
-
-                playlist_title  = content[j].content_title;
-                content_title   = content[j].content_title;
-                content_description = content[j].content_resume;
+                if(j == 0){
+                    html += '<div class="content active" id="content-'+j+'">'
+                    html += '   <p class="date">18.01.2017</p>'
+                    html += '   <p class="link">';
+                    html += '       <a href=javascript:loadVideo("'+content[j].video_src+'","'+ content[j].content_cover+'","content-'+j+'")>'+content[j].content_title+'</a>';
+                    html += '   </p>';
+                    html += '   <p class="hide-description hide">'+content[j].content_description+'</p>';
+                    html += '</div>';
+                    playlist_title  = content[0].content_title;
+                    content_title   = content[0].content_title;
+                    content_description = content[0].content_resume;
+                    first_file = content[0].video_src;
+                    first_cover = content[0].content_cover;
+                }else{
+                    html += '<div class="content" id="content-'+j+'">'
+                    html += '   <p class="date">18.01.2017</p>'
+                    html += '   <p class="link">';
+                    html += '       <a href=javascript:loadVideo("'+content[j].video_src+'","'+ content[j].content_cover+'","content-'+j+'")>'+content[j].content_title+'</a>';
+                    html += '   </p>';
+                    html += '   <p class="hide-description hide">'+content[j].content_description+'</p>';
+                    html += '</div>';
+                }
 
                 /*html += '<td><p>'+content[j].content_cover+'</p></td>';
                  html += '<td><p>'+content[j].content_description+'</p></td>';
@@ -51,5 +64,58 @@ function getData(data){
             }
         }
         $('#contents').html(html);
+        $('#playlist_title').html(playlist_title);
+        $('#content_title').html(content_title);
+        $('#content_description').html(content_description);
+
+        jwplayer('myVideo').setup({
+            "key":"3q3t8iIybSxKCGGz24tDJPg0q1eMN2UwljLScelSCPY=",
+            "abouttext":"produzido por everstream.com.br",
+            "aboutlink":"http:\/\/www.everstream.com.br",
+            "skin":{
+                "name":"Everstream",
+                "active":"#fdb000",
+                "inactive":"#c0c0c0",
+                "background":"#fdb000"
+            },
+            "flashplayer":"\/\/ssl.p.jwpcdn.com\/player\/v\/7.6.1\/jwplayer.flash.swf",
+            "androidhls":true,
+            "hlshtml":false,
+            "sources":{"file": first_file},
+            "image":first_cover,
+            "sharing":{
+                "code":"%3Ciframe%20src%3D%22http%3A\/\/content.jwplatform.com\/players\/MEDIAID-ROkXCTWe.html%22%20width%3D%22480%22%20height%3D%22270%22%20frameborder%3D%220%22%20scrolling%3D%22auto%22%3E%3C\/iframe%3E",
+                "heading":"Compartilhar",
+                "link":null,
+                "sites":["facebook","twitter","googleplus"]
+            },
+            "width":"100%",
+            "aspectratio":"16:9",
+            "autostart":"false"
+        });
     }
+
+}
+
+
+function loadVideo(file, image, id_active) {
+    var playerInstance = jwplayer("myVideo");
+    playerInstance.load([{
+        file: file,
+        image: image
+    }]);
+
+    $('#contents .content').each(function(){
+        el = $(this);
+        if(el.attr('id') == id_active && !el.hasClass('active')){
+            el.addClass('active')
+        }else if(el.attr('id') != id_active){
+            el.removeClass('active')
+        }
+        $('#playlist_title').html($('#'+id_active+' a').text());
+        $('#content_title').html($('#'+id_active+' a').text());
+        $('#content_description').html($('#'+id_active+' p.hide-description').text());
+    })
+
+    playerInstance.play();
 }
