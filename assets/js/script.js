dc0118f54eaa986943ebfbb267f6f3b49802b165
rgomesdn <rodrigo@iocode.com.br>
2017-02-03 15:18:53 -0200

    Adicionado modal com form de login

--- assets/js/script.js
+++ assets/js/script.js
@@ -119,3 +119,35 @@ function loadVideo(file, image, id_active) {
     playerInstance.play();
     $('html, body').animate({scrollTop:0}, 'slow');
 }
+
+$(document).ready(function(){
+    var url = location.href;
+    if(url.substring(url.indexOf('#')) == '#register'){
+        var id = '#login';
+
+        var alturaTela = $(document).height();
+        var larguraTela = $(window).width();
+
+        //colocando o fundo preto
+        $('#mascara').css({'width':larguraTela,'height':alturaTela});
+        $('#mascara').fadeIn(1000);
+        $('#mascara').fadeTo("slow",0.8);
+
+        var left = ($(window).width() /2) - ( $(id).width() / 2 );
+        var top = ($(window).height() / 2) - ( $(id).height() / 2 );
+
+        $(id).css({'top':top,'left':left});
+        $(id).show();
+    };
+
+    $("#mascara").click( function(){
+        $(this).hide();
+        $(".window").hide();
+    });
+
+    $('.fechar').click(function(ev){
+        ev.preventDefault();
+        $("#mascara").hide();
+        $(".window").hide();
+    });
+});
