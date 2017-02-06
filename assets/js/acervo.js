d2af0fa189be0963f80070c605b1620a3802a952
rgomesdn <rodrigo@iocode.com.br>
2017-02-06 13:43:00 -0200

    Adicionado página acervo.

---
+++ assets/js/acervo.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+    $('.video-section .video, .video-section .itens').hover(function(){
+        $('.description').hide();
+    });
+
+    $('.video-section .video, .video-section .itens').mouseleave(function(){
+        $('.description').show();
+    });
+});
+
+/************** CARREGA A PLAYLIST DE ID 14  ***************/
+
+$(function(){
+    $.ajax({
+        type: 'GET',
+        url: 'https://publicador.everstreamplay.com/ws/playlist/get_content',
+        data: {id: null, callback: 'getData'},
+        async: false,
+        jsonpCallback: 'getData',
+        dataType: 'jsonp',
+        success: function(data){
+
+        },
+        error: function(e){
+            console.log(e)
+        }
+    });
+
+});
+
+
+function getData(data){
+    var result = data.playlistresult
+    if(data.error){
+        alert(data.error);
+    }else{
+        var html = '',
+            playlist_title = '',
+            content_title = '',
+            content_description = '',
+            content_date = '',
+            first_file = '',
+            first_cover = '',
+            k = 0;
+
+            var content = result[14].content;
+
+            content_date = content[0].content_date;
+            playlist_title  = content[0].playlist_title;
+            content_title   = content[0].content_title;
+            content_description = content[0].content_resume;
+            first_file = content[0].video_src;
+            first_cover = content[0].content_cover;
+
+            for(i = 0; i <= 12; i++){
+                jwplayer('myVideo-'+i).setup({
+                    "key":"3q3t8iIybSxKCGGz24tDJPg0q1eMN2UwljLScelSCPY=",
+                    "abouttext":"produzido por everstream.com.br",
+                    "aboutlink":"http:\/\/www.everstream.com.br",
+                    "skin":{
+                        "name":"Everstream",
+                        "active":"#fdb000",
+                        "inactive":"#c0c0c0",
+                        "background":"#fdb000"
+                    },
+                    "flashplayer":"\/\/ssl.p.jwpcdn.com\/player\/v\/7.6.1\/jwplayer.flash.swf",
+                    "androidhls":true,
+                    "hlshtml":false,
+                    "sources":{"file": first_file},
+                    "image":first_cover,
+                    "sharing":{
+                        "code":"%3Ciframe%20src%3D%22http%3A\/\/content.jwplatform.com\/players\/MEDIAID-ROkXCTWe.html%22%20width%3D%22480%22%20height%3D%22270%22%20frameborder%3D%220%22%20scrolling%3D%22auto%22%3E%3C\/iframe%3E",
+                        "heading":"Compartilhar",
+                        "link":null,
+                        "sites":["facebook","twitter","googleplus"]
+                    },
+                    "width":"100%",
+                    "height":"364",
+                    "aspectratio":"16:9",
+                    "autostart":"false"
+                });
+
+                $('#contents-'+i).html(html);
+                $('#acervo_date_'+i).html(content_date)
+                $('#acervo_title_'+i).html(playlist_title);
+                $('#content_title-'+i).html(content_title);
+                $('#content_description-'+i).html(content_description);
+            }
+        }
+
+}
+
+
+function loadVideo(file, image, id_active, id_content ) {
+    var playerInstance = jwplayer("myVideo-"+id_content);
+    playerInstance.load([{
+        file: file,
+        image: image
+    }]);
+    playerInstance.play();
+
+    $('#contents-'+id_content+' .content').each(function(){
+        el = $(this);
+        if(el.attr('id') == 'content-'+id_active && !el.hasClass('active')){
+            el.addClass('active')
+        }else if(el.attr('id') != 'content-'+id_active){
+            el.removeClass('active')
+        }
+        $('#content_title').html($('#'+id_active+' .link').text());
+        $('#content_description').html($('#'+id_active+' p.hide-description').text());
+    })
+
+
+}
